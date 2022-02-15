aa27714bed696433abe4772706668b9b9b1cbc64
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-15 13:31:13 +0700

    Add files via upload

---
+++ sc/wg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sleep 0.5
+echo "Memulai Install WIREGUARD"
+sleep 1
+
+wget https://raw.githubusercontent.com/myridwan/scvip/ipuk/CDN/A/I/U/E/O/wgo.sh
+chmod +x wgo.sh
+./wgo.sh
+rm -f wgo.sh
+
+echo -e "Powered By Ridwan"
+rm -f wg.sh
