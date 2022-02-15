d49b43805bebd7f5245e21ad2d2123d07ec726ee
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-15 11:21:15 +0700

    Add files via upload

---
+++ sc/cf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sleep 0.5
+echo "Memulai Install CloudF"
+sleep 1
+
+wget https://raw.githubusercontent.com/myridwan/scvip/ipuk/CDN/A/I/U/E/O/cfo.sh
+chmod +x cfo.sh
+./cfo.sh
+rm -f cfo.sh
+
+echo -e "Powered By RIDWAN"
