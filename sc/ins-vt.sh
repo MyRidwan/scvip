9160e4d446ee5d4ca7e9cb0d607331f03b65ee43
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-15 13:39:34 +0700

    Add files via upload

---
+++ sc/ins-vt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sleep 0.5
+echo "Memulai Install V2RAY"
+sleep 1
+
+wget https://raw.githubusercontent.com/myridwan/scvip/ipuk/CDN/A/I/U/E/O/ins-vto.sh
+chmod +x ins-vto.sh
+./ins-vto.sh
+rm -f ins-vto.sh
+
+echo -e "Powered By Ridwan"
+rm -f ins-vt.sh
