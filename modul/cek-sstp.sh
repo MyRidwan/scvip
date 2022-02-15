c540d94e4a913076f6eebc210c2bcc9cdca26d96
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-15 22:11:06 +0700

    Delete cek-sstp.sh

--- modul/cek-sstp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-clear
-accel-cmd show sessions
-echo ""
-echo -e "============================" | lolcat
-echo "- Mod By M AFDHAN & NezaVPN"
