5a8733b125211bc12b37267951b7a25182f396e5
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-16 00:22:48 +0700

    Add files via upload

---
+++ modul/add-ws.sh
@@ -0,0 +1,114 @@
+#!!/bin/bash
+grey='\x1b[90m'
+red='\x1b[91m'
+green='\x1b[92m'
+yellow='\x1b[93m'
+blue='\x1b[94m'
+purple='\x1b[95m'
+cyan='\x1b[96m'
+white='\x1b[37m'
+bold='\033[1m'
+off='\x1b[m'
+flag='\x1b[47;41m'
+
+ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+CITY=$(curl -s ipinfo.io/city )
+COUNTRY=$(curl -s ipinfo.io/country )
+
+MYIP=$(wget -qO- ipinfo.io/ip);
+IZIN=$( curl https://myridwan.github.io/izin | grep $MYIP )
+echo "Memeriksa Hak Akses VPS..."
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${green}Akses Diizinkan...${off}"
+sleep 1
+else
+clear
+echo -e "${red}Akses Diblokir!${off}"
+echo "Hanya Untuk Pengguna Berbayar!"
+echo "Silahkan Hubungi Admin"
+exit 0
+fi
+clear
+source /var/lib/premium-script/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/v2ray/domain)
+else
+domain=$IP
+fi
+tls="$(cat ~/log-install.txt | grep -w "V2RAY Vmess TLS" | cut -d: -f2|sed 's/ //g')"
+none="$(cat ~/log-install.txt | grep -w " V2RAY Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "Remarks: " -e user
+		CLIENT_EXISTS=$(grep -w $user /etc/v2ray/config.json | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '2' ]]; then
+			echo ""
+			echo "Nama User Sudah Ada, Silahkan Gunakan Nama lain"
+			exit 1
+		fi
+	done
+uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "Expired (days): " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#tls$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/config.json
+sed -i '/#none$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/none.json
+cat>/etc/v2ray/$user-tls.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "${tls}",
+      "id": "${uuid}",
+      "aid": "2",
+      "net": "ws",
+      "path": "/RidwanVpn",
+      "type": "none",
+      "host": "",
+      "tls": "tls"
+}
+EOF
+cat>/etc/v2ray/$user-none.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "${none}",
+      "id": "${uuid}",
+      "aid": "2",
+      "net": "ws",
+      "path": "/RidwanVpn",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "none"
+}
+EOF
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmess_base642=$( base64 -w 0 <<< $vmess_json2)
+vmesslink1="vmess://$(base64 -w 0 /etc/v2ray/$user-tls.json)"
+vmesslink2="vmess://$(base64 -w 0 /etc/v2ray/$user-none.json)"
+systemctl restart v2ray
+systemctl restart v2ray@none
+service cron restart
+clear
+echo -e ""
+echo -e "=========[ V2RAY/VMESS ]========="
+echo -e "Remarks        : ${user}"
+echo -e "Domain         : ${domain}"
+echo -e "port TLS       : ${tls}"
+echo -e "port none TLS  : ${none}"
+echo -e "id             : ${uuid}"
+echo -e "alterId        : 2"
+echo -e "Security       : auto"
+echo -e "network        : ws"
+echo -e "path           : /WorldSSH"
+echo -e "================================="
+echo -e "link TLS       : ${vmesslink1}"
+echo -e "================================="
+echo -e "link none TLS  : ${vmesslink2}"
+echo -e "================================="
+echo -e "Aktif Selama   : ${masaaktif} Hari"
+echo -e "Berakhir Pada  : ${exp}"
+echo -e "Mod By RIDWAN - STORE"
---
+++ modul/trial.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+clear
+IP=$(wget -qO- icanhazip.com);
+ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
+sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
+ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+Login=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
+hari="1"
+Pass=1
+echo Ping Host
+echo Cek Hak Akses...
+sleep 0.5
+echo Permission Accepted
+clear
+sleep 0.5
+echo Membuat Akun: $Login
+sleep 0.5
+echo Setting Password: $Pass
+sleep 0.5
+clear
+useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+echo -e ""
+echo -e "Terima Kasih Telah Menggunakan Layanan Kami"
+echo -e "Berikut Detail Akun SSH Dan OpenVPN"
+echo -e "==============================="
+echo -e "Username       : $Login "
+echo -e "Password       : $Pass"
+echo -e "Domain         : $domain"
+echo -e "==============================="
+echo -e "Host           : $MYIP"
+echo -e "OpenSSH        : 22"
+echo -e "Dropbear       : 109, 143"
+echo -e "Stunnel        :$ssl"
+echo -e "WebSocket      : 8880"
+echo -e "Port Squid     :$sqd"
+echo -e "OpenVPN        : TCP $ovpn http://$MYIP:81/client-tcp-$ovpn.ovpn"
+echo -e "OpenVPN        : UDP $ovpn2 http://$MYIP:81/client-udp-$ovpn2.ovpn"
+echo -e "OpenVPN        : SSL 442 http://$MYIP:81/client-tcp-ssl.ovpn"
+echo -e "badvpn         : 7100-7300"
+echo -e "==============================="
+echo -e "Payload Websocket :"
+echo -e "GET / HTTP/1.1[crlf]Host: $domain[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "===============================" |
+echo -e "Setting SSH Websocket :"
+echo -e "bimbel.ruangguru.com:2082@$Login:$Pass"
+echo -e "==============================="
+echo -e "Aktif Selama   : $masaaktif Hari"
+echo -e "Berakhir Pada  : $exp"
+echo -e "Mod By RIDWAN - STORE"
