7b7bd649b54c7ae4674d0bac6b3425f7ae0f2a96
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-15 12:26:33 +0700

    Update vpn.sh

--- CDN/A/I/U/E/O/vpn.sh
+++ CDN/A/I/U/E/O/vpn.sh
@@ -11,7 +11,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://github.com/myridwan/scvip/raw/ipuk/vpn.zip
+wget https://github.com/myridwan/scvip/raw/ipuk/sc/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
