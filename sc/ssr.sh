08d5357db7f6f0a1be5a3d215a8a2b9ad7963ada
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-15 12:44:15 +0700

    Add files via upload

---
+++ sc/ssr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sleep 0.5
+echo "Memulai Install SSR"
+sleep 1
+
+wget https://raw.githubusercontent.com/myridwan/scvip/ipuk/CDN/A/I/U/E/O/ssro.sh
+chmod +x ssro.sh
+./ssro.sh
+rm -f ssro.sh
+
+echo -e "Powered By Ridwan"
+rm -f ssr.sh
