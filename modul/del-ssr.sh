d73df3a66b1f62915d458f583b0922f2f8e32bb9
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-15 17:17:52 +0700

    Add files via upload

---
+++ modul/del-ssr.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+cyan='\x1b[96m'
+white='\x1b[37m'
+bold='\033[1m'
+off='\x1b[m'
+
+clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/usr/local/shadowsocksr/akun.conf")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		clear
+		echo ""
+		echo "Anda Tidak Memiliki Klien!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo " Pilih Klien Yang Ingin Diperbarui"
+	echo " Klik CTRL+C untuk return"
+	echo -e "${cyan}===============================${off}"
+	echo "     No User Expired  "
+	grep -E "^### " "/usr/local/shadowsocksr/akun.conf" | cut -d ' ' -f 2-3 | nl -s ') '
+	echo -e "${cyan}===============================${off}"
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Pilih Salah Satu [1]: " CLIENT_NUMBER
+		else
+			read -rp "Pilih Salah Satu [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
+
+	# match the selected number to a client name
+	CLIENT_NAME=$(grep -E "^### " "/usr/local/shadowsocksr/akun.conf" | cut -d ' ' -f 2-3 | sed -n "${CLIENT_NUMBER}"p)
+	user=$(grep -E "^### " "/usr/local/shadowsocksr/akun.conf" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+	exp=$(grep -E "^### " "/usr/local/shadowsocksr/akun.conf" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+
+	# remove [Peer] block matching $CLIENT_NAME
+	sed -i "/^### $CLIENT_NAME/d" "/usr/local/shadowsocksr/akun.conf"
+cd /usr/local/shadowsocksr
+match_del=$(python mujson_mgr.py -d -u "${user}"|grep -w "delete user")
+cd
+service cron restart
+/etc/init.d/ssrmu restart
+clear
+echo " Akun Shadowsocks-R Berhasil Dihapus"
+echo " ==========================" | lolcat
+echo " Nama : $user"
+echo " Exp  : $exp"
+echo " ==========================" | lolcat
