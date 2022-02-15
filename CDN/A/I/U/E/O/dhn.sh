436504a5c02cbdea8107855bf7d6f3adadbf6843
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-15 11:44:23 +0700

    Add files via upload

---
+++ CDN/A/I/U/E/O/dhn.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+subb=$(cat /root/dom)
+DOMAIN=premium-server.xyz
+SUB_DOMAIN=*.${subb}.premium-server.xyz
+CF_ID=afdhan134@gmail.com
+CF_KEY=57fc95a923222474d5b90ff5444e0ee6f19ef
+set -euo pipefail
+IP=$(wget -qO- ipinfo.io/ip);
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+    
+rm -f /root/dom
